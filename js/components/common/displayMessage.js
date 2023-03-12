70a1d2c4ac5a6eb00672432d2c3bb8b8119226b6
xTidewaterx <79268288+xTidewaterx@users.noreply.github.com>
2023-03-12 02:52:14 +0100

    Delete js directory

--- js/components/common/displayMessage.js
+++
@@ -1,5 +0,0 @@
-export default function displayMessage(messageType, message, targetElement) {
-    const element = document.querySelector(targetElement);
-
-    element.innerHTML = `<div class="message ${messageType}">${message}</div>`;
-}
--- js/utils/favFunction.js
+++
@@ -1,9 +0,0 @@
-export function getExistingFavs() {
-    const favs = localStorage.getItem("favourites");
-
-    if (favs === null) {
-        return [];
-    } else {
-        return JSON.parse(favs);
-    }
-}
